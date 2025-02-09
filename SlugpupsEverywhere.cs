f9ba7d1cf779a1fb9dd95c35746823a726db1a81
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2025-02-09 14:28:16 -0300

    initial commit

---
+++ SlugpupsEverywhere.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+using MoreSlugcats;
+using System.Collections.Generic;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace SlugpupsEverywhere;
+
+[BepInPlugin(GUID, Name, Version)]
+public partial class SlugpupsEverywhere : BaseUnityPlugin
+{
+    public const string GUID = "Peroconino.SlugpupsEverywhere";
+    public const string Version = "1.0.0";
+    public const string Name = "Pups Everywhere";
+    private readonly SlugpupsEverywhereOptions? Options;
+    private bool IsInit;
+    public SlugpupsEverywhere()
+    {
+        try
+        {
+            Options = new SlugpupsEverywhereOptions(this, Logger);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+        }
+    }
+    public void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        if (IsInit) return;
+
+        try
+        {
+            IsInit = true;
+
+            //Your hooks go here
+            On.World.SpawnPupNPCs += hook_SpawnPupNPCs;
+
+            MachineConnector.SetRegisteredOI(GUID, Options);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+        }
+    }
+    public int hook_SpawnPupNPCs(On.World.orig_SpawnPupNPCs _orig, World self)
+    {
+        if (self.game.world.singleRoomWorld || self.game.rainWorld.safariMode || self.game.wasAnArtificerDream || self.game.GetStorySession.Players.Count == 0)
+        {
+            return 0;
+        }
+
+        int numOfAlivePups = 0;
+        int origNumOfAlivePups = 0;
+        AbstractRoom currentPlayerRoom = self.game.GetStorySession.Players[0].Room;
+        foreach (AbstractCreature abstractCreature in currentPlayerRoom.creatures)
+        {
+            if (abstractCreature.state.alive && abstractCreature.creatureTemplate.type == MoreSlugcatsEnums.CreatureTemplateType.SlugNPC)
+            {
+                numOfAlivePups++;
+                origNumOfAlivePups++;
+            }
+        }
+        UnityEngine.Random.State state = UnityEngine.Random.state;
+        self.game.GetStorySession.SetRandomSeedToCycleSeed(self.region.regionNumber);
+        if (UnityEngine.Random.value >= CalculatePupSpawnChance(self.region.regionParams.slugPupSpawnChance))
+        {
+            Logger.LogInfo(
+                "No slugpups this cycle"
+            );
+            UnityEngine.Random.state = state;
+            return numOfAlivePups;
+        }
+
+        int slugPupMaxCount = CalculatePupNumber(self.game.GetStorySession.saveState);
+        if (numOfAlivePups < slugPupMaxCount)
+        {
+            List<AbstractRoom> listOfShelters = [];
+            foreach (AbstractRoom abstractRoom in self.abstractRooms)
+            {
+                if (abstractRoom != currentPlayerRoom && abstractRoom.shelter && abstractRoom.name != "SU_S05")
+                {
+                    listOfShelters.Add(abstractRoom);
+                }
+            }
+
+            int allowedNumOfPups = Options!.IsByPassAllowedNumOfPups.Value ? Options.AmountOfPups.Value : 1;
+            AbstractRoom shelterOrCurrentRoom;
+            if (self.game.GetStorySession.saveState.forcePupsNextCycle >= 1)
+            {//REVIEW - Deixando em >= 1 faz com que continue nascendo filhotes mesmo sendo que eles tenham sobrevivido a mais de 1 ciclo
+                shelterOrCurrentRoom = currentPlayerRoom;
+                allowedNumOfPups = slugPupMaxCount - numOfAlivePups;
+                self.game.GetStorySession.saveState.forcePupsNextCycle = 2;
+            }
+            else
+            {
+                if (listOfShelters.Count == 0)
+                {
+                    Logger.LogWarning(
+                        "No shelters for pup spawns"
+                    );
+                    return numOfAlivePups;
+                }
+                if (listOfShelters.Count == 1)
+                {
+                    Logger.LogWarning(
+                        "only a SINGLE shelter for pup spawns"
+                    );
+                    shelterOrCurrentRoom = listOfShelters[0];
+                }
+                else
+                {
+                    shelterOrCurrentRoom = listOfShelters[UnityEngine.Random.Range(0, listOfShelters.Count)];
+                }
+            }
+
+            for (int j = 0; j < allowedNumOfPups; j++)
+            {
+                AbstractCreature slugPup = new(self, StaticWorld.GetCreatureTemplate(MoreSlugcatsEnums.CreatureTemplateType.SlugNPC), null, new WorldCoordinate(shelterOrCurrentRoom.index, -1, -1, 0), self.game.GetNewID());
+                shelterOrCurrentRoom.AddEntity(slugPup);
+                if (shelterOrCurrentRoom.realizedRoom != null)
+                {
+                    slugPup.RealizeInRoom();
+                }
+                (slugPup.state as PlayerNPCState)!.foodInStomach = 1;
+                numOfAlivePups++;
+                if (listOfShelters.Count > 1)
+                    shelterOrCurrentRoom = listOfShelters[UnityEngine.Random.Range(0, listOfShelters.Count)];
+                Logger.LogInfo(
+                    "Created slugpup! " + slugPup + " at " + shelterOrCurrentRoom.name + " " + shelterOrCurrentRoom.index
+                );
+            }
+        }
+        UnityEngine.Random.state = state;
+        return origNumOfAlivePups;
+    }
+    private int CalculatePupNumber(SaveState saveState)
+    {
+        if (Options is null || !ModManager.MSC || !(saveState.progression.miscProgressionData.beaten_Gourmand_Full || MoreSlugcats.MoreSlugcats.chtUnlockSlugpups.Value))
+        {
+            return 0;
+        }
+
+        if (((saveState.saveStateNumber == SlugcatStats.Name.White) && !Options.AllowWhiteCampaignSlugpups.Value) || ((saveState.saveStateNumber == SlugcatStats.Name.Yellow) && !Options.AllowYellowCampaignSlugpups.Value) || ((saveState.saveStateNumber == SlugcatStats.Name.Red) && !Options.AllowRedCampaignSlugpups.Value) ||
+        ((saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Gourmand) && !Options.AllowGourmandCampaignSlugpups.Value) || ((saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Artificer) && !Options.AllowArtificerCampaignSlugpups.Value) || ((saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Rivulet) && !Options.AllowRivuletCampaignSlugpups.Value) ||
+        ((saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Spear) && !Options.AllowSpearmasterCampaignSlugpups.Value) || ((saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Saint) && !Options.AllowSaintCampaignSlugpups.Value))
+        {
+            return 0;
+        }
+
+        if (saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Sofanthiel)
+        {
+            return 1000;
+        }
+
+        if (saveState.saveStateNumber == SlugcatStats.Name.Yellow || saveState.saveStateNumber == SlugcatStats.Name.White || saveState.saveStateNumber == SlugcatStats.Name.Red ||
+        saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Gourmand || saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Artificer ||
+        saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Rivulet || saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Spear ||
+        saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Saint)
+        {
+            return Options.AmountOfPups.Value;
+        }
+
+        return 0;
+    }
+    private float CalculatePupSpawnChance(float origPupSpawnChance)
+    {
+        if (Options is null || !Options.IsCustomSlugpupSpawnChance.Value)
+        {
+            return origPupSpawnChance;
+        }
+
+        return Options.SlugpupSpawnChance.Value;
+    }
+}
---
+++ SlugpupsEverywhereOptions.cs
@@ -0,0 +1,95 @@
+using BepInEx.Logging;
+using Menu.Remix.MixedUI;
+using Menu.Remix.MixedUI.ValueTypes;
+using UnityEngine;
+
+namespace SlugpupsEverywhere;
+
+public class SlugpupsEverywhereOptions : OptionInterface
+{
+    private readonly ManualLogSource Logger;
+    public readonly Configurable<bool> AllowYellowCampaignSlugpups;
+    public readonly Configurable<bool> AllowWhiteCampaignSlugpups;
+    public readonly Configurable<bool> AllowRedCampaignSlugpups;
+    public readonly Configurable<bool> AllowGourmandCampaignSlugpups;
+    public readonly Configurable<bool> AllowArtificerCampaignSlugpups;
+    public readonly Configurable<bool> AllowRivuletCampaignSlugpups;
+    public readonly Configurable<bool> AllowSpearmasterCampaignSlugpups;
+    public readonly Configurable<bool> AllowSaintCampaignSlugpups;
+    public readonly Configurable<bool> IsCustomSlugpupSpawnChance;
+    public readonly Configurable<bool> IsByPassAllowedNumOfPups;
+    public readonly Configurable<int> AmountOfPups;
+    public readonly Configurable<float> SlugpupSpawnChance;
+    private UIelement[]? UIArrPlayerOptions;
+    public SlugpupsEverywhereOptions(SlugpupsEverywhere modInstance, ManualLogSource loggerSource)
+    {
+        Logger = loggerSource;
+        AmountOfPups = this.config.Bind<int>(nameof(AmountOfPups), 2, new ConfigAcceptableRange<int>(0, 1000));
+        SlugpupSpawnChance = this.config.Bind<float>(nameof(SlugpupSpawnChance), 1, new ConfigAcceptableRange<float>(0, 1));
+        AllowWhiteCampaignSlugpups = this.config.Bind<bool>(nameof(AllowWhiteCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        AllowYellowCampaignSlugpups = this.config.Bind<bool>(nameof(AllowYellowCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        AllowRedCampaignSlugpups = this.config.Bind<bool>(nameof(AllowRedCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        AllowGourmandCampaignSlugpups = this.config.Bind<bool>(nameof(AllowGourmandCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        AllowArtificerCampaignSlugpups = this.config.Bind<bool>(nameof(AllowArtificerCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        AllowRivuletCampaignSlugpups = this.config.Bind<bool>(nameof(AllowRivuletCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        AllowSpearmasterCampaignSlugpups = this.config.Bind<bool>(nameof(AllowSpearmasterCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        AllowSaintCampaignSlugpups = this.config.Bind<bool>(nameof(AllowSaintCampaignSlugpups), true, new ConfigAcceptableRange<bool>(false, true));
+        IsCustomSlugpupSpawnChance = this.config.Bind<bool>(nameof(IsCustomSlugpupSpawnChance), false, new ConfigAcceptableRange<bool>(false, true));
+        IsByPassAllowedNumOfPups = this.config.Bind<bool>(nameof(IsByPassAllowedNumOfPups), false, new ConfigAcceptableRange<bool>(false, true));
+    }
+    public override void Initialize()
+    {
+        var opTab = new OpTab(this, "Options");
+        this.Tabs =
+        [
+            opTab
+        ];
+
+        UIArrPlayerOptions =
+        [
+            new OpLabel(10f, 570f, "Options", true),
+            new OpLabel(10f, 540f, "Possible amount of pups to spawn in the region"),
+            new OpSlider(AmountOfPups, new Vector2(20f, 490f),0.5f),
+            new OpLabel(10f, 450f, "Bypass allowed numbers of pups in a region?"),
+            new OpCheckBox(IsByPassAllowedNumOfPups, new Vector2(270f, 450f)),
+            new OpLabel(10f, 410f, "Note: If you want to bypass the allowed number of pups in a region, you can set this to true. This will\n allow you to spawn as many pups as you want in a region."),
+            new OpLabel(10f, 370f, "Allow pups to spawn on:", true),
+            new OpLabel(10f, 340f, "Monk campaign"),
+            new OpCheckBox(AllowYellowCampaignSlugpups, new Vector2(10f, 310f)),
+            new OpLabel(10f, 280f, "Survivor campaign"),
+            new OpCheckBox(AllowWhiteCampaignSlugpups, new Vector2(10f, 250f)),
+            new OpLabel(160f, 340f, "Hunter campaign"),
+            new OpCheckBox(AllowRedCampaignSlugpups, new Vector2(160f, 310f)),
+            new OpLabel(160f, 280f, "Gourmand campaign"),
+            new OpCheckBox(AllowGourmandCampaignSlugpups, new Vector2(160f, 250f)),
+            new OpLabel(310f, 340f, "Artificer campaign"),
+            new OpCheckBox(AllowArtificerCampaignSlugpups, new Vector2(310f, 310f)),
+            new OpLabel(310f, 280f, "Rivulet campaign"),
+            new OpCheckBox(AllowRivuletCampaignSlugpups, new Vector2(310f, 250f)),
+            new OpLabel(460f, 340f, "Spearmaster campaign"),
+            new OpCheckBox(AllowSpearmasterCampaignSlugpups, new Vector2(460f, 310f)),
+            new OpLabel(460f, 280f, "Saint campaign"),
+            new OpCheckBox(AllowSaintCampaignSlugpups, new Vector2(460f, 250f)),
+            new OpLabel(10f, 190f, "Do you want a custom pup spawn chance?", true),
+            new OpCheckBox(IsCustomSlugpupSpawnChance,new Vector2(430f, 190f)),
+            new OpFloatSlider(SlugpupSpawnChance, new Vector2(20f, 140f), 360, 2),
+        ];
+        opTab.AddItems(UIArrPlayerOptions);
+    }
+    public override void Update()
+    {
+        if (UIArrPlayerOptions is null)
+        {
+            return;
+        }
+
+        if (((OpCheckBox)UIArrPlayerOptions[24]).GetValueBool())
+        {
+            ((OpFloatSlider)UIArrPlayerOptions[25]).Show();
+        }
+        else
+        {
+            ((OpFloatSlider)UIArrPlayerOptions[25]).Hide();
+        }
+    }
+}
