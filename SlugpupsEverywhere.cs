5034a62711290c405a91d8bdf26814b87097b5df
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2025-02-16 14:48:11 -0300

    adding compability with Pup Karma

--- SlugpupsEverywhere.cs
+++ SlugpupsEverywhere.cs
@@ -5,6 +5,8 @@ using BepInEx;
 using MoreSlugcats;
 using System.Collections.Generic;
 using RWCustom;
+using PupKarma;
+using System.Reflection;
 
 #pragma warning disable CS0618
 
@@ -20,7 +22,7 @@ public partial class SlugpupsEverywhere : BaseUnityPlugin
     public const string Version = "1.0.0";
     public const string Name = "Pups Everywhere";
     private readonly SlugpupsEverywhereOptions? Options;
-    private bool IsInit;
+    private bool IsInit, hasPupKarmaMod;
     public SlugpupsEverywhere()
     {
         try
@@ -44,8 +46,20 @@ public partial class SlugpupsEverywhere : BaseUnityPlugin
         try
         {
             IsInit = true;
+            hasPupKarmaMod = false;
+            foreach (ModManager.Mod mod in ModManager.ActiveMods)
+            {
+                if (mod.name == "PupKarma")
+                {
+                    hasPupKarmaMod = true;
+                    break;
+                }
+            }
 
-            On.World.SpawnPupNPCs += hook_SpawnPupNPCs;
+            if (hasPupKarmaMod)
+                On.World.SpawnPupNPCs += hook_SpawnPupNPCsPupKarma;
+            else
+                On.World.SpawnPupNPCs += hook_SpawnPupNPCs;
 
             MachineConnector.SetRegisteredOI(GUID, Options);
         }
@@ -54,7 +68,7 @@ public partial class SlugpupsEverywhere : BaseUnityPlugin
             Logger.LogError(ex);
         }
     }
-    public int hook_SpawnPupNPCs(On.World.orig_SpawnPupNPCs _orig, World self)
+    public int hook_SpawnPupNPCsPupKarma(On.World.orig_SpawnPupNPCs _orig, World self)
     {
         if (self.game.world.singleRoomWorld || self.game.rainWorld.safariMode || self.game.wasAnArtificerDream || self.game.GetStorySession.Players.Count == 0)
         {
@@ -72,6 +86,7 @@ public partial class SlugpupsEverywhere : BaseUnityPlugin
                 origNumOfAlivePups++;
             }
         }
+
         UnityEngine.Random.State state = UnityEngine.Random.state;
         self.game.GetStorySession.SetRandomSeedToCycleSeed(self.region.regionNumber);
         if (UnityEngine.Random.value >= CalculatePupSpawnChance(self.region.regionParams.slugPupSpawnChance))
@@ -114,6 +129,7 @@ public partial class SlugpupsEverywhere : BaseUnityPlugin
                     );
                     return numOfAlivePups;
                 }
+
                 if (listOfShelters.Count == 1)
                 {
                     if (ModManager.DevTools)
@@ -126,6 +142,114 @@ public partial class SlugpupsEverywhere : BaseUnityPlugin
                 {
                     shelterOrCurrentRoom = listOfShelters[UnityEngine.Random.Range(0, listOfShelters.Count)];
                 }
+
+            }
+
+            for (int j = 0; j < allowedNumOfPups; j++)
+            {
+                AbstractCreature slugPup = new(self, StaticWorld.GetCreatureTemplate(MoreSlugcatsEnums.CreatureTemplateType.SlugNPC), null, new WorldCoordinate(shelterOrCurrentRoom.index, -1, -1, 0), self.game.GetNewID());
+                if (slugPup.TryGetPupData(out PupData pupData))
+                {
+                    pupData.AssignKarmaToPup();
+                }
+
+                shelterOrCurrentRoom.AddEntity(slugPup);
+                if (shelterOrCurrentRoom.realizedRoom != null)
+                {
+                    slugPup.RealizeInRoom();
+                }
+
+                (slugPup.state as PlayerNPCState)!.foodInStomach = 1;
+                numOfAlivePups++;
+                if (listOfShelters.Count > 1)
+                    shelterOrCurrentRoom = listOfShelters[UnityEngine.Random.Range(0, listOfShelters.Count)];
+
+                if (ModManager.DevTools)
+                    Logger.LogInfo(
+                         "Created slugpup! " + slugPup + " at " + shelterOrCurrentRoom.name + " " + shelterOrCurrentRoom.index
+                     );
+
+            }
+        }
+
+        UnityEngine.Random.state = state;
+        return origNumOfAlivePups;
+    }
+    public int hook_SpawnPupNPCs(On.World.orig_SpawnPupNPCs _orig, World self)
+    {
+        if (self.game.world.singleRoomWorld || self.game.rainWorld.safariMode || self.game.wasAnArtificerDream || self.game.GetStorySession.Players.Count == 0)
+        {
+            return 0;
+        }
+
+        int numOfAlivePups = 0;
+        int origNumOfAlivePups = 0;
+        AbstractRoom currentPlayerRoom = self.game.GetStorySession.Players[0].Room;
+        foreach (AbstractCreature abstractCreature in currentPlayerRoom.creatures)
+        {
+            if (abstractCreature.state.alive && abstractCreature.creatureTemplate.type == MoreSlugcatsEnums.CreatureTemplateType.SlugNPC)
+            {
+                numOfAlivePups++;
+                origNumOfAlivePups++;
+            }
+        }
+
+        UnityEngine.Random.State state = UnityEngine.Random.state;
+        self.game.GetStorySession.SetRandomSeedToCycleSeed(self.region.regionNumber);
+        if (UnityEngine.Random.value >= CalculatePupSpawnChance(self.region.regionParams.slugPupSpawnChance))
+        {
+            if (ModManager.DevTools)
+                Logger.LogInfo(
+                "No slugpups this cycle"
+            );
+            UnityEngine.Random.state = state;
+            return numOfAlivePups;
+        }
+
+        int slugPupMaxCount = CalculatePupNumber(self.game.GetStorySession.saveState);
+        if (numOfAlivePups < slugPupMaxCount)
+        {
+            List<AbstractRoom> listOfShelters = [];
+            foreach (AbstractRoom abstractRoom in self.abstractRooms)
+            {
+                if (abstractRoom != currentPlayerRoom && abstractRoom.shelter && abstractRoom.name != "SU_S05")
+                {
+                    listOfShelters.Add(abstractRoom);
+                }
+            }
+
+            int allowedNumOfPups = Options!.IsByPassAllowedNumOfPups.Value ? Options.AmountOfPups.Value : 1;
+            AbstractRoom shelterOrCurrentRoom;
+            if (self.game.GetStorySession.saveState.forcePupsNextCycle >= 1)
+            {//REVIEW - Deixando em >= 1 faz com que continue nascendo filhotes mesmo sendo que eles tenham sobrevivido a mais de 1 ciclo
+                shelterOrCurrentRoom = currentPlayerRoom;
+                allowedNumOfPups = slugPupMaxCount - numOfAlivePups;
+                self.game.GetStorySession.saveState.forcePupsNextCycle = 2;
+            }
+            else
+            {
+                if (listOfShelters.Count == 0)
+                {
+                    if (ModManager.DevTools)
+                        Logger.LogWarning(
+                        "No shelters for pup spawns"
+                    );
+                    return numOfAlivePups;
+                }
+
+                if (listOfShelters.Count == 1)
+                {
+                    if (ModManager.DevTools)
+                        Logger.LogWarning(
+                        "only a SINGLE shelter for pup spawns"
+                    );
+                    shelterOrCurrentRoom = listOfShelters[0];
+                }
+                else
+                {
+                    shelterOrCurrentRoom = listOfShelters[UnityEngine.Random.Range(0, listOfShelters.Count)];
+                }
+
             }
 
             for (int j = 0; j < allowedNumOfPups; j++)
@@ -136,16 +260,20 @@ public partial class SlugpupsEverywhere : BaseUnityPlugin
                 {
                     slugPup.RealizeInRoom();
                 }
+
                 (slugPup.state as PlayerNPCState)!.foodInStomach = 1;
                 numOfAlivePups++;
                 if (listOfShelters.Count > 1)
                     shelterOrCurrentRoom = listOfShelters[UnityEngine.Random.Range(0, listOfShelters.Count)];
+
                 if (ModManager.DevTools)
                     Logger.LogInfo(
                          "Created slugpup! " + slugPup + " at " + shelterOrCurrentRoom.name + " " + shelterOrCurrentRoom.index
                      );
+
             }
         }
+
         UnityEngine.Random.state = state;
         return origNumOfAlivePups;
     }
